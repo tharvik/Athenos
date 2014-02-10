a73ae7a95b5279d47b2208aad749cd26b42291cb
Kéwan Marconnet <tharvik@gmail.com>
2014-02-10 16:53:14 +0100

    Remove slick to make it compile

--- app/controllers/CourseController.scala
+++ app/controllers/CourseController.scala
@@ -4,8 +4,8 @@ import play.api.mvc.{Action, Controller}
 import play.api.libs.json._
 import play.api.Routes
 
-import scala.slick.driver.H2Driver.simple._
-import database._
+//import scala.slick.driver.H2Driver.simple._
+//import database._
 
 // TODO handle string in set* with spaces
 // TODO find a better time class
@@ -32,84 +32,80 @@ object CourseController extends Controller {
   implicit val courseWrites = Json.format[Course]
 
   // main part
-  val courses = TableQuery[Courses]
+  var courses: Map[String,Course] =
+    Map("Scala" -> Course("Scala",Professor("Martin Odersky"),Schedule(List(
+      Period("course", Time(0, 1200, 1400), 1, "CO1"))), "Scala course"))
 
-  Database.forURL("jdbc:h2:mem:hello", driver = "org.h2.Driver").withSession {
-    implicit session =>
-
-    courses.ddl.create
-
-    def getData(course: String) = Action {
-      courses.get(course) match {
-        case Some(c) => Ok(Json.toJson(c))
-        case _ => BadRequest("Not found in DB")
-      }
+  def getData(course: String) = Action {
+    courses.get(course) match {
+      case Some(c) => Ok(Json.toJson(c))
+      case _ => BadRequest("Not found in DB")
     }
+  }
 
-    // name
-    def getName(course: String) = Action {
-      courses.get(course) match {
-        case Some(c) => Ok(Json.toJson(c.name))
-        case _ => BadRequest("Not found in DB")
-      }
+  // name
+  def getName(course: String) = Action {
+    courses.get(course) match {
+      case Some(c) => Ok(Json.toJson(c.name))
+      case _ => BadRequest("Not found in DB")
     }
+  }
 
-    def setName(course: String, name: String) = Action {
-      courses.get(course) match {
-        case Some(c) => {
-          val old = courses(course)
-          courses = courses - course
-          courses = courses + (name -> new Course(name, old.professor,
-                                                      old.schedule,
-                                                      old.description))
-          Ok
-        } case _ => BadRequest("Not found in DB")
-      }
+  def setName(course: String, name: String) = Action {
+    courses.get(course) match {
+      case Some(c) => {
+        val old = courses(course)
+        courses = courses - course
+        courses = courses + (name -> new Course(name, old.professor,
+                                                    old.schedule,
+                                                    old.description))
+        Ok
+      } case _ => BadRequest("Not found in DB")
     }
+  }
 
-    // professor
-    def getProfessor(course: String) = Action {
-      courses.get(course) match {
-        case Some(c) => Ok(Json.toJson(c.professor))
-        case _ => BadRequest("Not found in DB")
-      }
+  // professor
+  def getProfessor(course: String) = Action {
+    courses.get(course) match {
+      case Some(c) => Ok(Json.toJson(c.professor))
+      case _ => BadRequest("Not found in DB")
     }
+  }
 
-    def setProfessor(course: String, name: String) = Action {
-      courses.get(course) match {
-        case Some(c) => {
-          val old = courses(course)
-          courses = courses - course
-          courses = courses + (course -> new Course(old.name, Professor(name),
-                                                      old.schedule,
-                                                      old.description))
-          Ok
-        } case _ => BadRequest("Not found in DB")
-      }
+  def setProfessor(course: String, name: String) = Action {
+    courses.get(course) match {
+      case Some(c) => {
+        val old = courses(course)
+        courses = courses - course
+        courses = courses + (course -> new Course(old.name, Professor(name),
+                                                    old.schedule,
+                                                    old.description))
+        Ok
+      } case _ => BadRequest("Not found in DB")
     }
+  }
 
-    // schedule
-    def getSchedule(course: String) = Action {
-      courses.get(course) match {
-        case Some(c) => Ok(Json.toJson(c.schedule))
-        case _ => BadRequest("Not found in DB")
-      }
+  // schedule
+  def getSchedule(course: String) = Action {
+    courses.get(course) match {
+      case Some(c) => Ok(Json.toJson(c.schedule))
+      case _ => BadRequest("Not found in DB")
     }
+  }
 
-    def addPeriod(course: String, json: String) = Action {
-      courses.get(course) match {
-        case Some(c) => {
-          val old = courses(course)
-          //TODO unescape string
-          val period: Period = Json.parse(json).as[Period]
-          courses = courses - course
-          courses = courses + (course -> new Course(old.name, old.professor,
-                                                      Schedule(
-                                                        period :: old.schedule.times
-                                                      ), old.description))
-          Ok
-        } case _ => BadRequest("Not found in DB")
-      }
+  def addPeriod(course: String, json: String) = Action {
+    courses.get(course) match {
+      case Some(c) => {
+        val old = courses(course)
+        //TODO unescape string
+        val period: Period = Json.parse(json).as[Period]
+        courses = courses - course
+        courses = courses + (course -> new Course(old.name, old.professor,
+                                                    Schedule(
+                                                      period :: old.schedule.times
+                                                    ), old.description))
+        Ok
+      } case _ => BadRequest("Not found in DB")
     }
   }
 }
--- app/controllers/Tables.scala
+++ app/controllers/Tables.scala
@@ -5,7 +5,7 @@ import scala.slick.lifted.{ProvenShape, ForeignKeyQuery}
 
 import controllers.CourseController._
 
-class Courses(tag: Tag) extends Table[(String, Professor, Schedule, String)](tag, "courses") {
+/*class Courses(tag: Tag) extends Table[(String, Professor, Schedule, String)](tag, "courses") {
   def name = Column[String]("name")
   def professor = Column[Professor]("professor")
   def schedule = Column[Schedule]("schedule")
@@ -13,4 +13,4 @@ class Courses(tag: Tag) extends Table[(String, Professor, Schedule, String)](tag
 
   // Every table needs a * projection with the same type as the table's type parameter
   def * : ProvenShape[(String, Professor, Schedule, String)] = (name, professor, schedule, description)
-}
+}*/
